2fc56ef164038f35e124449fe6b99ed84c8a1fba
Yuliia <littlehappinies@gmail.com>
2021-12-21 20:42:25 +0200

    Add burger button

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+(() => {
+    const menuBtnRef = document.querySelector('[data-burger-button]');
+    const mobileMenueRef = document.querySelector('[data-burger]');
+
+    menuBtnRef.addEventListener('Click', () => {
+        const expanded =
+            menuBtnRef.getAttribute('aria-expanded') === "true" || false;
+        
+        menuBtnRef.classList.toggle('is-open');
+        menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+        mobileMenueRef.classList.toggle('is-open');
+
+  })
+})();

57415c176ca811e826177e2383f319686ffffa53
Yuliia <littlehappinies@gmail.com>
2021-12-25 10:58:48 +0200

    Update burger menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-burger-button]");
     const mobileMenueRef = document.querySelector("[data-burger-menu]");
+    const mobileLogoRef = document.querySelector("[data-burger-logo]");
 
     menuBtnRef.addEventListener("click", () => {
         const expanded =
@@ -10,6 +11,8 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
         mobileMenueRef.classList.toggle("is-open");
+
+        mobileLogoRef.classList.toggle("is-open");
 
     });
 })();
